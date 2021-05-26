be94db1390c15a5f4a47be65de630b902c5580c3
Nikita Snitko <nsnitko2002nsnitko@gmail.com>
2021-05-26 11:36:17 +0300

    Рандомнi числа

--- Modulna/Source.cpp
+++ Modulna/Source.cpp
@@ -1,4 +1,18 @@
 #include <iostream>;
 using namespace std;
 int main() {
+	srand(time(0));
+	int dob = 1, sum = 0, float ser = 0;
+	float ex;
+	int amount;
+	cin >> amount;
+	for (int i = 1; i <= amount; i++) {
+		ex = rand() % 10 + 1;
+		sum += ex;
+		dob *= ex;
+		ser = ser + (ex / amount);
+	}
+	cout << "Summa - " << sum;
+	cout << "Dobutok - " << dob;
+	cout << "Seredne arifmetichne - " << ser;
 }
